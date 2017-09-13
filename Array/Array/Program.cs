ca2d1951ffbe0ba3ef5a910257d3cf4613617da4
Luis Mercado Castelli <memofunster@gmail.com>
2017-09-13 09:32:02 -0300

    Demo Arrays

---
+++ Array/Array/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new Item[5];
+            Random r = new Random();
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = new Item(r.Next());
+            }
+
+            Console.WriteLine("Items: ");
+
+            foreach (Item item in items)
+            {
+                Console.WriteLine("ItemsID = {0}", item.ID);
+            }
+
+            var Authors = new String[5];
+            Authors[0] = "Faulkner";
+            Authors[1] = "Proust";
+            Authors[2] = "Stephenson";
+            Authors[3] = "Gibson";
+            Authors[4] = "Melville";
+
+            Console.WriteLine("Authors...");
+            foreach (string s in Authors)
+            {
+                Console.WriteLine(s);
+            }
+            
+            Console.WriteLine("Authors...");
+
+
+
+
+        }
+
+    }
+}
