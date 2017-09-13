b33e54579d611d8583cba57fd2b7c35975c43acd
Luis Mercado Castelli <memofunster@gmail.com>
2017-09-13 09:50:54 -0300

    Demo Excepciones

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primera linea del programa");
+
+            var tester = new Tester();
+            tester.Metodo1();
+
+            //var x = 0;
+            //var y = 12 / x;           //Arroja excepcion de una division por cero
+            Console.WriteLine("Fin de programa"); //no alcanza a pasar por este write
+        }
+    }
+}
---
+++ Excepciones/Excepciones/Tester.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class Tester
+    {
+        public void Metodo1()
+        {
+            Console.WriteLine("Comienza Metodo 1");
+            Metodo2();
+            Console.WriteLine("Termina Metodo 1");
+        }
+
+        public void Metodo2()
+        {
+            Console.WriteLine("Comienza Metodo 2");
+            Metodo3();
+            Console.WriteLine("Termina Metodo 2");
+        }
+
+        public void Metodo3()
+        {
+            Console.WriteLine("Comienza Metodo 3");
+            try
+            {
+                var x = 0;
+                var y = 12 / x; 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Excepcion detectada: {0}", e.Message);
+            }
+          
+            Console.WriteLine("Termina Metodo 3");
+        }
+    }
+}
